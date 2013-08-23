24c52228549770703ac65013499298ce3d32f336
Alexandre <alexandre@kohen.fr>
2013-08-23 21:19:31 +0200

    Main

--- src/merkurius/ld27/pong/Pong.java
+++ src/merkurius/ld27/pong/Pong.java
@@ -9,8 +9,9 @@ public class Pong {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Merkurius LD27 Pong warmup";
 		cfg.useGL20 = true;
-		cfg.width = 640;
-		cfg.height = 480;
+		cfg.width = 800;
+		cfg.height = 600;
+		cfg.resizable = false;
 		
 		new LwjglApplication(new PongController(), cfg);
 	}
