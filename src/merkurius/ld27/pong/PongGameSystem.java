755d5eea72a2138fee458c22a558228f982ba114
GabrielPlassard <gabriel.plassard@gmail.com>
2013-08-24 01:28:00 +0200

    Ready again!!!!

--- src/merkurius/ld27/pong/PongGameSystem.java
+++ src/merkurius/ld27/pong/PongGameSystem.java
@@ -2,7 +2,6 @@ package merkurius.ld27.pong;
 
 import com.artemis.Entity;
 import com.artemis.systems.VoidEntitySystem;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 
 import fr.kohen.alexandre.framework.components.TextComponent;
@@ -13,8 +12,8 @@ public class PongGameSystem extends VoidEntitySystem {
 
     private int scoreLeft = 0;
     private int scoreRight = 0;
-    private StringBuffer bufferScoreLeft;
-    private StringBuffer bufferScoreRight;
+    private TextComponent componentScoreLeft;
+    private TextComponent componentScoreRight;
 
     private Entity ball;
     private Transform ballTransform;
@@ -22,15 +21,14 @@ public class PongGameSystem extends VoidEntitySystem {
 
     private boolean gameOn = false;
 
-
     @Override
     public void initialize(){
         Entity leftDisplay = EntityFactoryPong.newScoreDisplay(world,1,50,270,"Right");
         Entity rightDisplay = EntityFactoryPong.newScoreDisplay(world,1,-50,270,"Left");
         leftDisplay.addToWorld();
         rightDisplay.addToWorld();
-        bufferScoreLeft = leftDisplay.getComponent(TextComponent.class).text;
-        bufferScoreRight = rightDisplay.getComponent(TextComponent.class).text;
+        componentScoreLeft = leftDisplay.getComponent(TextComponent.class);
+        componentScoreRight = rightDisplay.getComponent(TextComponent.class);
 
         ball = EntityFactoryPong.newBall(world,1,0,0);
         ball.addToWorld();
@@ -40,34 +38,26 @@ public class PongGameSystem extends VoidEntitySystem {
 
     @Override
     protected void processSystem() {
-
         if (!gameOn){
             setBallOnMiddleAndApplyStrength();
             gameOn = true;
         }
         if (ballTransform.getPosition2().x < -380){
-            bufferScoreRight = new StringBuffer(""+scoreRight++);
+            componentScoreRight.text = new StringBuffer(""+(++scoreRight));
             setBallOnMiddleAndApplyStrength();
-            displayScores();
         }
         else if (ballTransform.getPosition2().x > 380){
-            scoreLeft = new StringBuffer(Integer.parseInt(scoreLeft.toString()) + 1);
+            componentScoreLeft.text = new StringBuffer(""+(++scoreLeft));
             setBallOnMiddleAndApplyStrength();
-            displayScores();
         }
-    }
-
-    private void displayScores(){
-        System.out.println(scoreLeft+", "+scoreRight+" / "+bufferScoreLeft+", "+bufferScoreRight);
     }
 
     private void setBallOnMiddleAndApplyStrength(){
-        ballTransform.setPosition(new Vector2(0,0));
-        ballVelocity.setSpeed(250, 0);
-        Gdx.app.log("GameSystem", "start");
-        gameOn = true;
-//        float angle = Math.random()
-//        ballBody.applyForceToCenter( new Vector2(0,1).setAngle(45));
-
+        ballTransform.setPosition(new Vector2(0, 0));
+        float angle = (float) (-30.0 + Math.random() * 60);
+        Vector2 force = new Vector2(0,250);
+        force.setAngle(angle);
+        boolean goRight = Math.random() > 0.5;
+        ballVelocity.setSpeed(goRight? force.x : -force.x, force.y);
     }
 }
